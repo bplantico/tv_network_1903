dee7b67082530309686da8b51d142fb634a8a375
Brian Plantico <bplantico@gmail.com>
2019-03-27 09:17:48 -0600

    Adds iteration 1

--- test/character_test.rb
+++ test/character_test.rb
@@ -3,23 +3,29 @@ require 'minitest/emoji'
 require './lib/character'
 
 class CharacterTest < Minitest::Test
-  
 
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
 
+    assert_instance_of Character, kitt
+  end
 
+  def test_name_method_can_be_called
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "KITT", kitt.name
+  end
+
+  def test_actor_method_can_be_called
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "William Daniels", kitt.actor
+  end
+
+  def test_salary_method_can_be_called
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal 1000000, kitt.salary
+  end
 
 end
-# pry(main)> require './lib/character'
-# # => true
-#
-# pry(main)> kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-# # => #<Character:0x00007f98a4ba8dc8...>
-#
-# pry(main)> kitt.name
-# # => "KITT"
-#
-# pry(main)> kitt.actor
-# # => "William Daniels"
-#
-# pry(main)> kitt.salary
-# # => 1000000
